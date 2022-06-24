42af5c2db47d34473811a7482db6b3e5a7c9e1ba
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-24 02:50:01 +0200

    Zadanie 17

--- task.js
+++ task.js
@@ -1,14 +1,19 @@
-const book = {
-  title: "The Last Kingdom",
-  author: "Bernard Cornwell",
-  genres: ["historical prose", "adventure"],
-  rating: 8.38,
+const salaries = {
+  mango: 100,
+  poly: 150,
+  alfred: 80,
 };
-const keys = Object.keys(book);
 
-for (const key of keys) {
-  // Klucz
-  console.log(key);
-  // Wartość właściwości
-  console.log(book[key]);
+function countTotalSalary() {
+  
+  let totalSalary = 0;
+  const values = Object.values(salaries);
+
+  for (const value of values) {
+    totalSalary += value;
+    console.log(totalSalary);
 }
+
+}
+
+countTotalSalary();
