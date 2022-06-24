ebf7610b0a232e68a4ea94e16bc353d2d060158f
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-25 01:24:07 +0200

    Zadanie 19

--- task.js
+++ task.js
@@ -1,19 +1,26 @@
-
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "Sen śmiesznego człowieka",
+    author: "Fiodor Dostojewski",
+    rating: 10.1,
+  },
 ];
 
-let productName = "Radar";
-console.log(productName);
+let totalRating = 0;
 
-let prices = [0,3,2];
-//prices = Object.values(products.price);
-console.log (prices);
+for (const book of books) {
+  totalRating += book.rating;
+}
 
-//let index = ([products.name.indexOf(productName)]);
-console.log (index);
-
-// console.log (prices[index]);
+const averageRating = (totalRating / books.length).toFixed(2);
+console.log(averageRating); // 8.2
