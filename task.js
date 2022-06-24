28ab6f8bc2e1f5284aee5d8ffdee330debfeed71
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-24 04:18:28 +0200

    Zadanie 18

--- task.js
+++ task.js
@@ -1,19 +1,19 @@
-const salaries = {
-  mango: 100,
-  poly: 150,
-  alfred: 80,
-};
 
-function countTotalSalary() {
-  
-  let totalSalary = 0;
-  const values = Object.values(salaries);
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
 
-  for (const value of values) {
-    totalSalary += value;
-    console.log(totalSalary);
-}
+let productName = "Radar";
+console.log(productName);
 
-}
+let prices = [0,3,2];
+//prices = Object.values(products.price);
+console.log (prices);
 
-countTotalSalary();
+//let index = ([products.name.indexOf(productName)]);
+console.log (index);
+
+// console.log (prices[index]);
