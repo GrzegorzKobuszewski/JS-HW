a07a68b70a57a26dfdd0cb6de23b75e26da1d2ba
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-26 00:53:55 +0200

    Zadanie 24

--- task.js
+++ task.js
@@ -1,21 +1,19 @@
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
-];
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Change code below this line
 
-function calculateTotalPrice(productName) {
-  // Пиши код ниже этой строки
-let totalPrice = 0;
-  for (const product of products) {
-    if (product.name === productName) {
-      totalPrice = product.price * product.quantity
-    }
-  }
-  return totalPrice;
-  // Пиши код выше этой строки
-}
-
-console.log(calculateTotalPrice("Radar"));
-console.log("Sprawdzenie:", products[0].price*products[0].quantity);
+const {
+  today:{high:highToday, low:lowToday, 
+        icon:todayIcon="https://www.flaticon.com/svg/static/icons/svg/861/861059.svg"},
+  tomorrow:{high:highTomorrow, low:lowTomorrow,
+        icon:tomorrowIcon="https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"},
+      }=forecast;
