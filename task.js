1cac303bcad05322d215da69022092e9084c07e0
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-27 01:14:42 +0200

    Zadanie 29

--- task.js
+++ task.js
@@ -1,11 +1,16 @@
-const firstGroupScores = [64, 42, 93];
-const secondGroupScores = [89, 14, 51, 26];
-const thirdGroupScores = [29, 47, 18, 97, 81];
+const defaultSettings = {
+  theme: "light",
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
 // Change code below this line
-const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
-const bestScore = Math.max(...allScores);
-const worstScore = Math.min(...allScores);
+const finalSettings = {...defaultSettings,...overrideSettings};
 
-console.log (allScores);
-console.log (bestScore);
-console.log (worstScore);
+console.log (finalSettings);
