259c98aec8c7260c4d3b30235f3cd1e998966f6a
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-26 02:19:01 +0200

    Zadanie 26

--- task.js
+++ task.js
@@ -1,19 +1,8 @@
-const forecast = {
-  today: {
-    low: 28,
-    high: 32,
-    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
-  },
-  tomorrow: {
-    low: 27,
-    high: 31,
-  },
-};
 // Change code below this line
+function calculateMeanTemperature(forecast) {
+  const {today:{low:todayLow, high:todayHigh}, 
+         tomorrow:{low:tomorrowLow, high:tomorrowHigh}} = forecast;
 
-const {
-  today:{high:highToday, low:lowToday, 
-        icon:todayIcon="https://www.flaticon.com/svg/static/icons/svg/861/861059.svg"},
-  tomorrow:{high:highTomorrow, low:lowTomorrow,
-        icon:tomorrowIcon="https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"},
-      }=forecast;
+  // Change code above this line
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
