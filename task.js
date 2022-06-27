6ad830aa1372538fed418d32e2ad9123122560ea
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-27 04:17:56 +0200

    Zadanie 34

--- task.js
+++ task.js
@@ -1,15 +1,17 @@
-// Change code below this line
-function findMatches(array, ...args) {
-  const matches = []; // Don't change this line
-  const a = array.length;
-      for (let arg of args) {
-        for (let i = 0; i < a; i+=1) {
-        if (array[i] == arg) { matches.push(arg) }
-      }
-  }
-  console.log (matches);
+const bookShelf = {
+  // Change code below this line
+  books: ["The last kingdom", "The guardian of dreams"],
+  getBooks() {
+    return "Returning all books";
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook(oldName) {
+    return `Deleting book ${oldName}`;
+  },
+    updateBook(oldName, newName) {
+    return `Updating book ${oldName} to ${newName}`;
+  },  
   // Change code above this line
-  return matches;
-}
-
-findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41);
+};
