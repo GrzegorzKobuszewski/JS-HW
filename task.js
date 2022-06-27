825a1055f13733d691d7e0b841cb54e94c34e786
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-27 03:03:03 +0200

    Zadanie 31

--- task.js
+++ task.js
@@ -1,9 +1,15 @@
-function makeTask(data) {
-  const completed = false;
-  const category = "General";
-  const priority = "Normal";
-  // Change code below this line
-  const object = { category, priority, completed, ...data};
-  return object;
+// Change code below this line
+function add(...args) {
+  let add = 0;
+  const array = [];
+  for (const arg of args) {
+    array.push(arg);
+    add += arg;
+  }
+    console.log(array);
+  console.log(`Suma elementów tablicy wynosi: ${ add }`);
+  return;
   // Change code above this line
 }
+  
+  add(74, 11, 62, 46, 12, 36);
