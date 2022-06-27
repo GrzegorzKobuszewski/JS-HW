7ee055a99485b61cd20b838767b146fbcc3b51de
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-27 16:48:44 +0200

    Zadanie 37

--- task.js
+++ task.js
@@ -1,18 +1,13 @@
-const bookShelf = {
-  books: ["The last kingdom", "Haze", "The guardian of dreams"],
-  updateBook(oldName, newName) {
-    // Change code below this line
-    console.log(this.books);
-    const indexBook = this.books.indexOf(oldName);
-
-    let array = this.books.splice(indexBook, 1, newName);
-    console.log (this.books);
-    return;
-    // Change code above this line
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
   },
+
+  // Change code above this line
 };
 
-bookShelf.updateBook("The last kingdom", "Dune");
-
+console.log (atTheOldToad.getPotions());
 
 
