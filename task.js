812c79ed0a4615cb773406ca37af5cb36e7d9ae8
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-27 03:15:20 +0200

    Zadanie 32

--- task.js
+++ task.js
@@ -1,15 +1,12 @@
 // Change code below this line
-function add(...args) {
-  let add = 0;
-  const array = [];
+function addOverNum(a, ...args) {
+  let total = 0;
+
   for (const arg of args) {
-    array.push(arg);
-    add += arg;
+    if (a < arg)
+        total += arg;
   }
-    console.log(array);
-  console.log(`Suma elementów tablicy wynosi: ${ add }`);
-  return;
+
+  return total;
   // Change code above this line
 }
-  
-  add(74, 11, 62, 46, 12, 36);
