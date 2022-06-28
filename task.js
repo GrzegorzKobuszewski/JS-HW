4178e6ce1297748fdbf50219adb65051300fb947
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-28 17:36:52 +0200

    Zadanie 41

--- task.js
+++ task.js
@@ -1,13 +1,48 @@
 const atTheOldToad = {
-  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
   // Change code below this line
+
   getPotions() {
     return this.potions;
   },
 
+
+  addPotion(newPotion) {
+    for (const object of this.potions) {
+    if (object.name === newPotion.name) {
+      return `Error! Potion ${newPotion.name} is already in your inventory!`;
+    }
+    }
+   return this.potions.push(newPotion);
+  },
+
+
+  removePotion(potionName) {
+    const potionIndex = this.potions.indexOf(potionName);
+    for (const object of this.potions) {
+    if (potionIndex === -1) {
+      return `Potion ${potionName} is not in inventory!`;
+    }
+}
+    this.potions.splice(potionIndex, 1);
+  },
+
+
+
+
   // Change code above this line
 };
 
-console.log (atTheOldToad.getPotions());
 
+
+
+console.log(atTheOldToad.addPotion({ name: "Speed222", price: 460 }));
+
+
+const names = atTheOldToad.potions.map(potions => potions.name);
+console.log(names);
 
