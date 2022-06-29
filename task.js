830ce5dd246ef6f017ae8f8dbb7d966df76ad5ec
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-06-29 03:22:20 +0200

    Zadanie 5m4
    forEach

--- task.js
+++ task.js
@@ -1,33 +1,27 @@
-const pizzaPalace = {
-  pizzas: ["Ultracheese", "Smoked", "Four meats"],
-  order(pizzaName, onSuccess, onError) {
+const numbers = [5, 10, 15, 20, 25];
 
-    if (!pizzaPalace.pizzas.includes(pizzaName)) {
-      return onError(`There is no pizza with a name ${pizzaName} in the assortment.`)
-    };
-    return onSuccess(pizzaName);
-  },
-};
-// Change code above this line
+let total = 0;
 
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// Iterujący forEach
+  numbers.forEach(function (number, index) {
+    total += number;
+  console.log(`Indeks ${index}, wartość ${number}`);
+});
+
+console.log(total);
+
+
+
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Change code below this line
+
+  orderedItems.forEach(function (number) {
+  totalPrice += number;
+  });
+
+  // Change code above this line
+  return totalPrice;
 }
 
-// Callback for onError
-function onOrderError(error) {
-  return `Error! ${error}`;
-}
-
-// Method calls with callbacks
-pizzaPalace.order("Smoked", makePizza, onOrderError);
-pizzaPalace.order("Four meats", makePizza, onOrderError);
-pizzaPalace.order("Big Mike", makePizza, onOrderError);
-pizzaPalace.order("Vienna", makePizza, onOrderError);
-
-//Wywołanie:
-console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
-console.log(pizzaPalace.order("Four meats", makePizza, onOrderError));
-console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
-console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
