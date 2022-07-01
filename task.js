5ed11f9bcffa4d5c706bbe3f1cc928358cc5bcd3
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-07-01 05:44:27 +0200

    Zadanie przykład

--- task.js
+++ task.js
@@ -1,23 +1,10 @@
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
+const employee = {
+  baseSalary: "RA ",
+  overtime: 10,
+  rate: 20,
+  getWage() {
+    return this.baseSalary + this.overtime * this.rate;
   },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
-];
-// Change code below this line
+};
 
-const titles = books.map(books=>books.title);
-console.log(titles);
+console.log (employee.getWage());
