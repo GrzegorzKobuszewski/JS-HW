3dcacaade2d3630f6438d58bf3eff2f5e021d8c3
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-09-13 22:36:10 +0200

    add array arr4

--- task2.js
+++ task2.js
@@ -1,12 +1,14 @@
 let arr1 = [1, 2, 3];
 let arr2 = arr1;
 let arr3 = arr2;
+let arr4 = arr3;
 
 arr3[0] = 0;
 
 console.log(arr1);
 console.log(arr2);
 console.log(arr3);
+console.log(arr4);
 
 arr1 = null;
 arr2 = undefined;
